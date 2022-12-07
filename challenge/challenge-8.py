b0eeb00f55d0a817ce1c41f3a6aea39342f00a74
Juanhen10 <91297117+Juanhen10@users.noreply.github.com>
2022-12-07 13:57:10 -0300

    Add files via upload

---
+++ challenge/challenge-8.py
@@ -0,0 +1,20 @@
+'''
+Exercícios
+Crie funções que duplicam, triplicam e quadruplicam o número recebido como parâmetro
+'''
+import os
+os.system('cls')
+
+def multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+    
+duplicar = multiplicador(2)
+triplicar = multiplicador(3)
+quadruplicar = multiplicador(4)
+
+for numero in range(5):
+    print(numero(duplicar))
